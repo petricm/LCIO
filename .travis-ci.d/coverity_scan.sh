5cd9aa79e8268a558a7d40bfdfff72331a426687
Marko Petric <marko.petric@cern.ch>
2016-12-16 13:50:10 +0100

    test

--- .travis-ci.d/coverity_scan.sh
+++ .travis-ci.d/coverity_scan.sh
@@ -6,17 +6,12 @@ source $ILCSOFT/init_ilcsoft.sh
 cd /Package
 mkdir build
 cd build
-cmake -DUSE_CXX11=ON -DBUILD_ROOTDICT=ON ..
-wget https://scan.coverity.com/download/linux64 --post-data "token=${COVERITY_SCAN_TOKEN}&project=iLCSoft%2FLCIO" -O coverity_tool.tgz
-mkdir cov-analysis-linux64
-tar -xf coverity_tool.tgz -C cov-analysis-linux64 --strip-components=2
-export PATH=$(pwd)/cov-analysis-linux64/bin:$PATH
-cov-build --dir cov-int make -j2
-tar czvf myproject.tgz cov-int
-export description=`date`
-curl --form token=${COVERITY_SCAN_TOKEN} \
-     --form email=marko.petric@cern.ch \
-     --form file=@myproject.tgz \
-     --form version="master" \
-     --form description="${description}" \
-     https://scan.coverity.com/builds?project=iLCSoft%2FLCIO
+cmake -DUSE_CXX11=ON -DBUILD_ROOTDICT=ON .. && \
+wget https://scan.coverity.com/download/linux64 --post-data "token=${COVERITY_SCAN_TOKEN}&project=iLCSoft%2FLCIO" -O coverity_tool.tgz && \
+mkdir cov-analysis-linux64 && \
+tar -xf coverity_tool.tgz -C cov-analysis-linux64 --strip-components=2 && \
+export PATH=$(pwd)/cov-analysis-linux64/bin:$PATH && \
+cov-build --dir cov-int make -j2 && \
+tar czvf myproject.tgz cov-int && \
+export description=`date` && \
+curl --form token=${COVERITY_SCAN_TOKEN} --form email=marko.petric@cern.ch --form file=@myproject.tgz --form version="master" --form description="${description}" https://scan.coverity.com/builds?project=iLCSoft%2FLCIO
