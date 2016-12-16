505527686927a7bf0012ab3b60650b03e851ee0a
Marko Petric <marko.petric@cern.ch>
2016-12-16 11:23:58 +0100

    test

--- .travis-ci.d/coverity_scan.sh
+++ .travis-ci.d/coverity_scan.sh
@@ -13,3 +13,10 @@ tar -xf coverity_tool.tgz -C cov-analysis-linux64 --strip-components=2
 export PATH=$(pwd)/cov-analysis-linux64/bin:$PATH
 cov-build --dir cov-int make -j2
 tar czvf myproject.tgz cov-int
+export description=`date`
+curl --form token=${COVERITY_SCAN_TOKEN} \
+     --form email=marko.petric@cern.ch \
+     --form file=@myproject.tgz \
+     --form version="master" \
+     --form description="${description}" \
+     https://scan.coverity.com/builds?project=iLCSoft%2FLCIO
