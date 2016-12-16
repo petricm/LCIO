c59b9c1dfd75a65bc5c40211a6ca4f23df48d36c
Marko Petric <marko.petric@cern.ch>
2016-12-16 14:06:14 +0100

    test

--- .travis-ci.d/coverity_scan.sh
+++ .travis-ci.d/coverity_scan.sh
@@ -6,7 +6,7 @@ source $ILCSOFT/init_ilcsoft.sh
 cd /Package
 mkdir build
 cd build
-cmake -DUSE_CXX11=ON -DBUILD_ROOTDICT=ON .. && \
+cmake -DUSE_CXX11=ON -DBUILD_ROOTDICT=OFF .. && \
 wget https://scan.coverity.com/download/linux64 --post-data "token=${COVERITY_SCAN_TOKEN}&project=iLCSoft%2FLCIO" -O coverity_tool.tgz && \
 mkdir cov-analysis-linux64 && \
 tar -xf coverity_tool.tgz -C cov-analysis-linux64 --strip-components=2 && \
