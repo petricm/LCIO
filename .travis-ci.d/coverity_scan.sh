6b62a2838216c89e945ace58a51c0c1a1feee263
Marko Petric <marko.petric@cern.ch>
2017-01-18 15:52:41 +0100

    new test4

--- .travis-ci.d/coverity_scan.sh
+++ .travis-ci.d/coverity_scan.sh
@@ -7,9 +7,6 @@ cd /Package
 mkdir build
 cd build
 cmake -DUSE_CXX11=ON -DBUILD_ROOTDICT=OFF .. && \
-mv ../coverity_tool.tgz . && \
-mkdir cov-analysis-linux64 && \
-tar -xf coverity_tool.tgz -C cov-analysis-linux64 --strip-components=2 && \
-export PATH=$(pwd)/cov-analysis-linux64/bin:$PATH && \
+export PATH=/Package/cov-analysis-linux64/bin:$PATH && \
 cov-build --dir cov-int make -j2 && \
 tar czvf myproject.tgz cov-int
