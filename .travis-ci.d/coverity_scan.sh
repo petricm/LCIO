1df9b2c1faab1ae549fa62fe720e59417b6693be
Marko Petric <marko.petric@cern.ch>
2017-01-18 15:05:04 +0100

    test coverity scan on cc7

--- .travis-ci.d/coverity_scan.sh
+++ .travis-ci.d/coverity_scan.sh
@@ -7,7 +7,7 @@ cd /Package
 mkdir build
 cd build
 cmake -DUSE_CXX11=ON -DBUILD_ROOTDICT=OFF .. && \
-wget https://scan.coverity.com/download/linux64 --post-data "token=${COVERITY_SCAN_TOKEN}&project=iLCSoft%2FLCIO" -O coverity_tool.tgz && \
+mv ../coverity_tool.tgz . && \
 mkdir cov-analysis-linux64 && \
 tar -xf coverity_tool.tgz -C cov-analysis-linux64 --strip-components=2 && \
 export PATH=$(pwd)/cov-analysis-linux64/bin:$PATH && \
